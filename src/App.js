ac2c597aac035311a109acdc49abe18c689bbc53
itKapitan <itkapitan@gmail.com>
2021-04-06 19:14:57 +0300

    Каталог фильмов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+import Movies from './Movie'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  getMovies = async() => {
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json')
+    this.setState({movies, isLoading: false})
+  }
+  componentDidMount() {
+    this.getMovies()
+  }
+
+  render() {
+    const {isLoading, movies} = this.state
+    return (
+      <section className="movies">
+        {isLoading
+          ? ( <div className="loader">загрузка...</div> )
+          : (
+            movies.map(movie => {
+              return (<Movies
+                id={movie.id}
+                title={movie.title}
+                summary={movie.summary}
+                cover={movie.large_cover_image}
+                year={movie.year}
+                genres={movie.genres}/>)
+            })
+          )}
+      </section>
+    )
+  }
 }
 
-export default App;
+export default App
